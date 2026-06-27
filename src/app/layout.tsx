a0afa50ab32cce8e2cba9dcc8e99cf46ba1ed03b
Shreeram Modi <smodi@smodi.net>
2026-06-26 22:39:50 -0700

    feat(fonts): bundle DM Sans/IBM Plex Mono/Inter and add flow font variable

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -70,6 +70,102 @@ const commitMono = localFont({
     display: "swap",
 });
 
+const dmSans = localFont({
+    src: [
+        {
+            path: "./fonts/dm-sans/dm-sans-latin-400-normal.woff2",
+            weight: "400",
+            style: "normal",
+        },
+        {
+            path: "./fonts/dm-sans/dm-sans-latin-400-italic.woff2",
+            weight: "400",
+            style: "italic",
+        },
+        {
+            path: "./fonts/dm-sans/dm-sans-latin-500-normal.woff2",
+            weight: "500",
+            style: "normal",
+        },
+        {
+            path: "./fonts/dm-sans/dm-sans-latin-600-normal.woff2",
+            weight: "600",
+            style: "normal",
+        },
+        {
+            path: "./fonts/dm-sans/dm-sans-latin-700-normal.woff2",
+            weight: "700",
+            style: "normal",
+        },
+    ],
+    variable: "--font-dm-sans",
+    display: "swap",
+});
+
+const ibmPlexMono = localFont({
+    src: [
+        {
+            path: "./fonts/ibm-plex-mono/ibm-plex-mono-latin-400-normal.woff2",
+            weight: "400",
+            style: "normal",
+        },
+        {
+            path: "./fonts/ibm-plex-mono/ibm-plex-mono-latin-400-italic.woff2",
+            weight: "400",
+            style: "italic",
+        },
+        {
+            path: "./fonts/ibm-plex-mono/ibm-plex-mono-latin-500-normal.woff2",
+            weight: "500",
+            style: "normal",
+        },
+        {
+            path: "./fonts/ibm-plex-mono/ibm-plex-mono-latin-600-normal.woff2",
+            weight: "600",
+            style: "normal",
+        },
+        {
+            path: "./fonts/ibm-plex-mono/ibm-plex-mono-latin-700-normal.woff2",
+            weight: "700",
+            style: "normal",
+        },
+    ],
+    variable: "--font-ibm-plex-mono",
+    display: "swap",
+});
+
+const inter = localFont({
+    src: [
+        {
+            path: "./fonts/inter/inter-latin-400-normal.woff2",
+            weight: "400",
+            style: "normal",
+        },
+        {
+            path: "./fonts/inter/inter-latin-400-italic.woff2",
+            weight: "400",
+            style: "italic",
+        },
+        {
+            path: "./fonts/inter/inter-latin-500-normal.woff2",
+            weight: "500",
+            style: "normal",
+        },
+        {
+            path: "./fonts/inter/inter-latin-600-normal.woff2",
+            weight: "600",
+            style: "normal",
+        },
+        {
+            path: "./fonts/inter/inter-latin-700-normal.woff2",
+            weight: "700",
+            style: "normal",
+        },
+    ],
+    variable: "--font-inter",
+    display: "swap",
+});
+
 export const metadata: Metadata = {
     title: {
         default: "ebb",
@@ -86,7 +182,10 @@ export default function RootLayout({
     children: React.ReactNode;
 }) {
     return (
-        <html lang="en" className={commitMono.variable}>
+        <html
+            lang="en"
+            className={`${commitMono.variable} ${dmSans.variable} ${ibmPlexMono.variable} ${inter.variable}`}
+        >
             <body className="font-sans antialiased">
                 {children}
                 <Toaster position="bottom-center" />
